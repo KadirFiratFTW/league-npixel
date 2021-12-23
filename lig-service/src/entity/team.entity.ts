c2eb3751c96b9e4cdb3597ccfce7d2a1e2bf20d5
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-12-23 23:13:52 +0300

    Update team.entity.ts

--- lig-service/src/entity/team.entity.ts
+++ lig-service/src/entity/team.entity.ts
@@ -1,12 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { Match } from "./match.entity";
+
 @Entity('teams')
 export class Team {
-
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
     teamName: string;
-
 }
