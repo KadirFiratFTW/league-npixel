d70f00fc9cba59b13f63cc4912939e45c44e7d10
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-12-23 23:19:10 +0300

    Update match.controller.ts

--- lig-api/src/match/match.controller.ts
+++ lig-api/src/match/match.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { MatchService } from "./match.service";
+
 @Controller('matches')
 export class MatchController {
-
-    constructor(
-        private matchService: MatchService,
-    ) {
-
-    }
+    constructor(private matchService: MatchService) {}
 
     @Get("run")
     async run() {
         return this.matchService.runActiveWeek();
     }
-
-
 }
